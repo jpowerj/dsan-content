d6afa525b69c921a9c163df80f68dd576325485a
Jeff Jacobs <jjacobs3@cs.stanford.edu>
2025-06-06 06:24:30 -0400

    Create corrections_cell.py

---
+++ corrections_cell.py
@@ -0,0 +1,15 @@
+# @title Assignment Corrections (Run Cell to Fetch / Refresh)
+import base64, datetime, json
+from urllib.request import urlopen
+from IPython.display import Markdown
+def fetch_github(gh_url):
+  md_data = json.load(urlopen(gh_url))
+  md_content_b64 = md_data['content']
+  md_content = base64.b64decode(md_content_b64).decode('utf-8')
+  return md_content
+exec_tz = datetime.datetime.now().astimezone().tzinfo
+exec_timestamp = datetime.datetime.now(tz=exec_tz).strftime("%Y-%m-%d %H:%M:%S %Z")
+corrections_url = "https://api.github.com/repos/jpowerj/dsan-content/contents/2025-sum-dsan5650/DSAN5650_HW1_Corrections.md?ref=main"
+md_content = fetch_github(corrections_url)
+corrections_content = md_content + f"\n\nLast fetched: {exec_timestamp}"
+Markdown(corrections_content)
